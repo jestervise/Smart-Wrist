d4898a722809a227b5cdef7029826e19a29be01d
mj <chang_mingjun@live.com>
2019-05-17 09:42:59 +0800

    before eject

--- components/Profile.js
+++ components/Profile.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput, Platform, TouchableOpacity, ImageBackground, Alert ,Dimensions} from 'react-native';
+import { View, Text, TextInput, Platform, TouchableOpacity, ImageBackground, Alert ,Dimensions,ScrollView} from 'react-native';
 import Icon from '@expo/vector-icons/Ionicons';
 import { ProfileCircle } from './SvgShapes';
 import firebase from './firebaseconfig';
 import { Calendar as RNCalendar } from 'react-native-calendars';
-import { Permissions, Location, ImagePicker, IntentLauncherAndroid as IntentLauncher } from 'expo';
+import { Permissions, Location, ImagePicker, IntentLauncherAndroid as IntentLauncher,LinearGradient } from 'expo';
 import { Overlay, Button as RNButton } from 'react-native-elements';
+
 var { height, width } = Dimensions.get("window");
 
 export class Profile extends Component {
@@ -138,11 +139,14 @@ export class Profile extends Component {
   render() {
     let location = "Loading";
     let image = this.state.image;
+    const themeColor = '#EFEBE6'
     //Format the location data to state and country
     if (this.state.location) {
       location = this.state.location[0].region + "," + this.state.location[0].country;
     }
-    return (<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    return (<ScrollView style={{ flex: 1,backgroundColor:themeColor }}>
+    <LinearGradient colors={["#fff","#FA9014"]}>
+
       {/* Top Left Overlay */}
       <Overlay onBackdropPress={() => { this.setState({ isVisible: false }); }} isVisible={this.state.isVisible} style={{ flex: 1, justifyContent: 'space-between' }}>
         <View>
@@ -165,26 +169,68 @@ export class Profile extends Component {
 
       </Overlay>
 
-      <ImageBackground source={this.state.image} style={{ top: 0, width: '100%', position: 'absolute', height: height * 0.4 }}>
+      <ImageBackground source={this.state.image} style={{ width: '100%',height: height * 0.4 ,backgroundColor:'red'}}>
         <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingTop: 20 }}>
           <CalenderComponent OpenCalender={this.OpenCalender.bind(this)} />
           <EditBanner ChoosePhoto={this.ChoosePhoto.bind(this)} />
         </View>
         <ProfileCircle />
       </ImageBackground>
+      <View style={{flexDirection:'row',alignItems:'center',justifyContent:'space-between'}}>
+        {/* Left indentation*/}
+        <View style={{flex:0.3,backgroundColor:'blue'}}/>
+        {/* The input name */}
+        <View style={{ flex:0.5,alignSelf:'flex-end', marginVertical:20,marginTop:20, }}>
+          <View style={{ flexDirection: 'row' }}>
+            <TextInput ref={this.textInput} value={this.state.displayName} editable={this.state.editable} onSubmitEditing={this.SaveDisplayName.bind(this)} onChangeText={(text) => this.setState({ displayName: text })} style={{ color: "#F68909", fontSize: 15, textAlign: "left", fontWeight: 'bold' }} />
+            <EditUserName ChangeDisplayName={this.ChangeDisplayName.bind(this)} />
+          </View>
+          <View style={{ flexDirection: 'row', paddingTop: 5, }}>
+            <Icon name="md-pin" size={12} color="#FF5353" />
+            <Text style={{ paddingLeft: 10, color: '#FF5353', fontSize: 12, textAlign: 'left' }}>{location}</Text>
+          </View>
+        </View>
+        {/* Right Button */}
+        <RNButton title="Thanos" titleStyle={{color:'#FF6A6A'}} buttonStyle={{backgroundColor:'transparent',shadowColor: "#919191",
+shadowOffset: {
+	width: 0,
+	height: 1,
+},
+shadowOpacity: 0.18,
+shadowRadius: 1.00,
 
-      <View style={{ top: '5%', marginTop: '5%', left: '10%', width: "65%" }}>
-        <View style={{ flexDirection: 'row' }}>
-          <TextInput ref={this.textInput} value={this.state.displayName} editable={this.state.editable} onSubmitEditing={this.SaveDisplayName.bind(this)} onChangeText={(text) => this.setState({ displayName: text })} style={{ color: "#F68909", fontSize: 15, textAlign: "left", fontWeight: 'bold' }} />
-          <EditUserName ChangeDisplayName={this.ChangeDisplayName.bind(this)} />
-        </View>
-        <View style={{ flexDirection: 'row', paddingTop: 5 }}>
-          <Icon name="md-pin" size={12} color="#FF5353" />
-          <Text style={{ paddingLeft: 10, color: '#FF5353', fontSize: 12, textAlign: 'left' }}>{location}</Text>
-        </View>
+elevation: 1,padding:20,paddingVertical:10,marginRight:20,borderRadius:10}} style={{flex:0.2,backgroundColor:'yellow',marginRight:10}}/>
       </View>
-    </View>);
+      <ReportFeed text="The hardest choices require the strongest wills"/>
+      <ReportFeed text="Fun isn’t something one considers when balancing the universe. But this… does put a smile on my face."/>
+      <ReportFeed text="When I’m done, half of humanity will still exist. Perfectly balanced, as all things should be. I hope they remember you."/>
+      <ReportFeed text="I know what it’s like to lose. To feel so desperately that you’re right, yet to fail nonetheless. Dread it. Run from it. Destiny still arrives. Or should I say, I have."/>
+    </LinearGradient>
+    </ScrollView>);
   }
+}
+
+const ReportFeed =(props)=>{
+  const themeColor='#EFEBE6'
+  const style={padding:20,backgroundColor:'#FF6A6A',width:'80%',height:'auto',
+  borderRadius: 10,alignItems: 'center',shadowColor: "#000",
+  shadowOffset: {
+    width: 0,
+    height: 2,
+  },
+  shadowOpacity: 0.23,
+  shadowRadius: 2.62,
+  
+  elevation: 4,}
+  return (
+    <View style={{alignItems:'center',marginBottom:20,justifyContent: 'center',}}>
+        <View style={style}>
+        <View style={{width:20,height:20,backgroundColor:themeColor,borderRadius:50,position:'absolute',left:-10,top:"60%"}}/>
+        <View><Text style={{color:themeColor}}>{props.text}</Text></View>
+          
+          </View>
+    </View>
+  )
 }
 
 //Profile's Function Component/Icons in Profile page
